367488517cb80083088ddc72854f5eb71c41dfd1
Nick Baroni <nick@rhythnic.com>
2016-01-29 07:53:20 -0500

    Refactored mixin to remove getState method.

--- example/riot-redux.js
+++ example/riot-redux.js
@@ -11,12 +11,27 @@ var ReduxMixin = (function (){
     return node;
   }
 
+  function updateState() {
+    var shouldUpdate = false;
+    var state = _store.getState();
+    var elementState = {};
+    this._reduxUsePaths.forEach(function (path) {
+      elementState[path[0]] = valueByPath(state, path[1]);
+      if (elementState[path[0]] !== this.state[path[0]]) {
+        shouldUpdate = true;
+      }
+    }, this);
+    if (shouldUpdate) {
+      this.update({state: elementState})
+    }
+  }
+
   riot.mixin('redux', {
     init: function () {
       this.store = _store;
       this.state = {};
       this.on('unmount', function () {
-        this._unsubscribe();
+        if (this._unsubscribe) this._unsubscribe();
       }.bind(this))
     },
     dispatch: function(type, data) {
@@ -26,22 +41,8 @@ var ReduxMixin = (function (){
       this._reduxUsePaths = Object.keys(paths).map(function (key) {
         return [key, paths[key].split('.')];
       });
-      this._unsubscribe = _store.subscribe(this.getState.bind(this));
-      this.getState();
-    },
-    getState: function () {
-      var shouldUpdate = false;
-      var state = _store.getState();
-      var elementState = {};
-      this._reduxUsePaths.forEach(function (path) {
-        elementState[path[0]] = valueByPath(state, path[1]);
-        if (elementState[path[0]] !== this.state[path[0]]) {
-          shouldUpdate = true;
-        }
-      }, this);
-      if (shouldUpdate) {
-        this.update({state: elementState})
-      }
+      this._unsubscribe = _store.subscribe(updateState.bind(this));
+      updateState.call(this);
     }
   })
 
